9eae398b205142f139eedc1aa4f7ccf44e81017d
LucianoPecile <luciano.pecile@hotmail.com>
2020-10-13 11:43:20 -0300

    Empezando con el login. Tipo de Documento corregido en el listado de personas

--- BuenaVista/urls.py
+++ BuenaVista/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import listarPersona, editarPersona, crearPersona, crearZona, eliminarPersona,iniciarSesion
+from core.views import listarPersona, editarPersona, crearPersona, crearZona, eliminarPersona, iniciarSesion, cerrarSesion
 from core.class_views import PersonaCreate,PersonaDelete,PersonaList,PersonaUpdate
 
 urlpatterns = [
 
     #Views
     path('admin/', admin.site.urls),
+    
     #path('listar_persona/',listarPersona,name = 'listar_persona'),
     #path('crear_persona/',crearPersona,name = 'crear_persona'),
     #path('editar_persona/<int:id>/',editarPersona,name = 'editar_persona'),
@@ -33,7 +34,7 @@ urlpatterns = [
     path('editar_persona/<int:pk>/',PersonaUpdate.as_view(),name = 'editar_persona'),
     path('eliminar_persona/<int:pk>/', PersonaDelete.as_view(), name = 'eliminar_persona'),
     path('iniciar_sesion/', iniciarSesion, name = 'iniciar_sesion'),
+    path('cerrar_sesion/', cerrarSesion, name = 'cerrar_sesion'),
     
-    
-    path('crear_zona/',crearZona,name = 'crear_zona'),
+    path('crear_zona/',crearZona,name = 'crear_zona')
 ]
