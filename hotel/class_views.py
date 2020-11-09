87ed9d3c3685f3f868ba8baf5f8182e98d1a8b19
ale2trelew <ale2trelew@gmail.com>
2020-11-09 11:01:26 -0300

    primer draft de gestion habitacion

--- hotel/class_views.py
+++ hotel/class_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView,ListView,DeleteView,UpdateView #View
 from django.urls import reverse_lazy
-from .forms import HotelForm
-from .models import Hotel
+from .forms import HotelForm, HabitacionForm
+from .models import Hotel, Habitacion
 
 #Hotel
 class HotelList(ListView):
@@ -25,3 +25,25 @@ class HotelDelete(DeleteView):
     model = Hotel
     template_name = 'verificacion.html'
     success_url = reverse_lazy('listar_hotel')
+
+#Habitacion
+class HabitacionList(ListView):
+    model = Habitacion
+    template_name = 'listar_habitacion.html'
+
+class HabitacionCreate(CreateView):
+    model = Habitacion
+    form_class = HabitacionForm
+    template_name = 'crear_habitacion.html'
+    success_url = reverse_lazy('listar_habitacion')
+
+class HabitacionUpdate(UpdateView):
+    model = Habitacion
+    form_class = HabitacionForm
+    template_name = 'crear_habitacion.html'
+    success_url = reverse_lazy('listar_habitacion')
+
+class HabitacionDelete(DeleteView):
+    model = Habitacion
+    template_name = 'verificacion.html'
+    success_url = reverse_lazy('listar_habitacion')
--- hotel/urls.py
+++ hotel/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from hotel.class_views import HotelCreate, HotelDelete, HotelList, HotelUpdate
+from hotel.class_views import HabitacionCreate, HabitacionDelete, HabitacionList, HabitacionUpdate
 #from .views import (aca ponen las funciones de views que quieran importar)
 #from .class_views import (aca ponen las funciones de class_views que quieran importar)
 
@@ -11,4 +12,9 @@ urlpatterns = [
     path('editar_hotel/<int:pk>/',HotelUpdate.as_view(),name = 'editar_hotel'),
     path('eliminar_hotel/<int:pk>/', HotelDelete.as_view(), name = 'eliminar_hotel'),
     
+    #Habitacion
+    path('listar_habitacion/',HabitacionList.as_view() ,name = 'listar_habitacion'),
+    path('crear_habitacion/',HabitacionCreate.as_view(),name = 'crear_habitacion'),
+    path('editar_habitacion/<int:pk>/',HabitacionUpdate.as_view(),name = 'editar_habitacion'),
+    path('eliminar_habitacion/<int:pk>/', HabitacionDelete.as_view(), name = 'eliminar_habitacion'),
 ]
