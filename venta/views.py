7a5c9d7395f6764daba2b671e74a122e455b2e52
LucianoPecile <luciano.pecile@hotmail.com>
2020-12-02 20:52:16 -0300

    .

--- venta/views.py
+++ venta/views.py
@@ -7,13 +7,18 @@ from hotel.models import Habitacion, Hotel
 
 def alquilar_habitacion(request):
     habitaciones = Habitacion.objects.all()
+    
+    # Parámetros de búsqueda:
     # localidad = NULL  # Filtro por defecto
     # fecha_desde
     # fecha_hasta
     # huespedes
 
+    #Esta es la parte de la búsqueda/filtro
     # if request.POST.get('localidad', '', '', ''):
     #     localidad = request.POST.get('localidad')
+    #     fecha_desde
+    #     fecha_hasta
     #     huespedes = int(request.POST.get('huespedes'))
     #     habitaciones = habitaciones.filter(localidad=localidad)
 
